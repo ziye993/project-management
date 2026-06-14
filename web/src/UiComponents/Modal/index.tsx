d99a8b467283bd9783a30c36d9866566851bf1db
wangke <2996447644@qq.com>
2026-06-14 22:58:48 +0800

    上传组件样式优化

--- web/src/UiComponents/Modal/index.tsx
+++ web/src/UiComponents/Modal/index.tsx
@@ -16,22 +16,29 @@ interface IProps {
   [key: string]: any;
 }
 
+const ANIMATION_DURATION = 500;
+
 const DomModal = (props: IProps) => {
   const { open, onClose, onOK } = props;
   const [renderModal, setRenderModal] = React.useState(open);
+  const [animatedOpen, setAnimatedOpen] = React.useState(false);
 
   useEffect(() => {
-    if (!props.open) {
-      setTimeout(() => {
-        setRenderModal(props.open)
-      }, 500)
-    } else {
-      setRenderModal(props.open)
+    if (open) {
+      setRenderModal(true);
+      const frame = requestAnimationFrame(() => {
+        requestAnimationFrame(() => setAnimatedOpen(true));
+      });
+      return () => cancelAnimationFrame(frame);
     }
+
+    setAnimatedOpen(false);
+    const timer = setTimeout(() => setRenderModal(false), ANIMATION_DURATION);
+    return () => clearTimeout(timer);
   }, [open]);
 
-  return (renderModal && <div className={styles.box} >
-    <div className={styles.content} style={{ width: props.width }}>
+  return (renderModal && <div className={`${styles.box} ${animatedOpen ? styles.boxOpen : styles.boxClose}`} >
+    <div className={`${styles.content} ${animatedOpen ? styles.contentOpen : styles.contentClose}`} style={{ width: props.width }}>
       <div className={styles.title}><div>{props.title || ""}</div> <span><CloseOutlined onClick={onClose} /></span></div>
       <div className={styles.userContent}>{props.children}</div>
       <div className={styles.footer}>
